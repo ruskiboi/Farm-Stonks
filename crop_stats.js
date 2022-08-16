8552dd23b8f09418d520ec063725be3cbd2bf9a1
ruskiboi <63880257+ruskiboi@users.noreply.github.com>
2022-08-16 11:41:49 -0500

    Add files via upload

---
+++ crop_stats.js
@@ -0,0 +1,28 @@
+
+//Crops
+function setCrops() {
+
+    const apple = { price: 1.25, sellValue: 1.50, growTime: 20, item_type: "crop", itemID: 01 };
+    const potato = { price: 0.50, sellValue: 0.75, growTime: 20, item_type: "crop", itemID: 02 };
+    const wheat = { price: 0.75, sellValue: 1.00, growTime: 20, item_type: "crop", itemID: 03 };
+    const beetroot = { price: 0.50, sellValue: 0.75, growTime: 20, item_type: "crop", itemID: 04 };
+    const tomato = { price: 1.00, sellValue: 1.50, growTime: 30, item_type: "crop", itemID: 05 };
+    const corn = { price: 0.75, sellValue: 1.00, growTime: 40, item_type: "crop", itemID: 06 };
+    const carrot = { price: 1.00, sellValue: 1.25, growTime: 20, item_type: "crop", itemID: 07 };
+    const watermelon = { price: 1.50, sellValue: 2.00, growTime: 50, item_type: "crop", itemID: 08 };
+    const pumpkin = { price: 1.25, sellValue: 1.75, growTime: 40, item_type: "crop", itemID: 09 };
+}
+
+
+//Manual Tilling Tools
+
+function 
+    const steel_trowel = { price: 0.00, sellValue: 0.00, toolLevel: 1, itemType: "manual_tools", itemID: 0 };
+    const titanium_trowel = { price: 0.00, sellValue: 0.00, toolLevel: 2, itemType: "manual_tools", itemID: 1 };
+    const wooden_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 3, itemType: "manual_tools", itemID: 2 };
+    const fiberglass_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 4, itemType: "manual_tools", itemID: 3 };
+    const aluiminum_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 5, itemType: "manual_tools", itemID: 4 };
+    const _ = {price: 0.00, sellValue: 0.00, toolLevel: 6, itemType: "manual_tools", itemID: 5};
+    const steel_trowel = {price: 0.00, sellValue: 0.00, toolLevel: 7, itemType: "manual_tools", itemID: 6};
+
+
---
+++ local_balance.js
@@ -0,0 +1,34 @@
+//local_balance stores and minipulates client-side balance data.
+
+//
+
+
+
+
+
+var userBal;
+
+
+
+//Functions
+
+function onFirstPlay() 
+{ 
+    localStorage.setItem("localStorageBal", 200);
+
+//localStorage Crop Stats
+    localStorage.setItem("lStorage_Apple", apple);
+    localStorage.setItem("lStorage_Potato", potato);
+    localStorage.setItem("lStorage_Wheat", wheat);
+    localStorage.setItem("lStorage_Beetroot", beetroot);
+    localStorage.setItem("lStorage_Tomato", tomato);
+    localStorage.setItem("lStorage_Corn", corn);
+    localStorage.setItem("lStorage_Carrot", carrot);
+    localStorage.setItem("lStorage_Watermelon", watermelon);
+    localStorage.setItem("lStorage_Pumpkin", pumpkin);
+
+//localStorage Tool Stats
+
+}
+
+
