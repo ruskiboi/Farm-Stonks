bf98cff540032adf5a3dd796fdc2a0889d7de6c6
ruskiboi <63880257+ruskiboi@users.noreply.github.com>
2022-08-19 12:09:15 -0500

    new commit 2

--- crop_stats.js
+++ crop_stats.js
@@ -1,5 +1,6 @@
 
 //Crops
+
 function setCrops() {
 
     const apple = { price: 1.25, sellValue: 1.50, growTime: 20, item_type: "crop", itemID: 01 };
@@ -11,6 +12,7 @@ function setCrops() {
     const carrot = { price: 1.00, sellValue: 1.25, growTime: 20, item_type: "crop", itemID: 07 };
     const watermelon = { price: 1.50, sellValue: 2.00, growTime: 50, item_type: "crop", itemID: 08 };
     const pumpkin = { price: 1.25, sellValue: 1.75, growTime: 40, item_type: "crop", itemID: 09 };
+
 };
 
 
@@ -18,14 +20,29 @@ function setCrops() {
 
 function setManualTillingTools() {
 
-    const steel_trowel = { price: 0.00, sellValue: 0.00, toolLevel: 1, itemType: "manual_tools", itemID: 0 };
-    const titanium_trowel = { price: 0.00, sellValue: 0.00, toolLevel: 2, itemType: "manual_tools", itemID: 1 };
-    const wooden_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 3, itemType: "manual_tools", itemID: 2 };
-    const fiberglass_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 4, itemType: "manual_tools", itemID: 3 };
-    const aluiminum_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 5, itemType: "manual_tools", itemID: 4 };
-    const carbon_fiber_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 6, itemType: "manual_tools", itemID: 5 };
-    const steel_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 7, itemType: "manual_tools", itemID: 6 };
+    const steel_trowel = { price: 0.00, sellValue: 0.00, toolLevel: 1, itemType: "manual_tilling_tools", itemID: 0 };
+    const titanium_trowel = { price: 0.00, sellValue: 0.00, toolLevel: 2, itemType: "manual_tilling_tools", itemID: 1 };
+    const wooden_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 3, itemType: "manual_tilling_tools", itemID: 2 };
+    const fiberglass_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 4, itemType: "manual_tilling_tools", itemID: 3 };
+    const aluiminum_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 5, itemType: "manual_tilling_tools", itemID: 4 };
+    const steel_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 6, itemType: "manual_tilling_tools", itemID: 5 };
+    const carbon_fiber_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 7, itemType: "manual_tilling_tools", itemID: 6 };
+    const titanium_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 8, itemType: "manual_tilling_tools", itemID: 7 };
 
 };
     
 
+//Manual Harvesting Tools
+
+function setManualHarvestingTools() {
+
+    const steel_sheers = { price: 0.00, sellValue: 0.00, toolLevel: 1, itemType: "manual_harvesting_tools", itemID: 0 };
+    const titanium_sheers = { price: 0.00, sellValue: 0.00, toolLevel: 2, itemType: "manual_harvesting_tools", itemID: 1 };
+    const wooden_sickle = { price: 0.00, sellValue: 0.00, toolLevel: 3, itemType: "manual_harvesting_tools", itemID: 2 };
+    const fiberglass_sickle = { price: 0.00, sellValue: 0.00, toolLevel: 4, itemType: "manual_harvesting_tools", itemID: 3 };
+    const aluiminum_sickle = { price: 0.00, sellValue: 0.00, toolLevel: 5, itemType: "manual_harvesting_tools", itemID: 4 };
+    const steel_sickle = { price: 0.00, sellValue: 0.00, toolLevel: 6, itemType: "manual_harvesting_tools", itemID: 5 };
+    const carbon_fiber_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 7, itemType: "manual_harvesting_tools", itemID: 6 };
+    const titanium_hoe = { price: 0.00, sellValue: 0.00, toolLevel: 8, itemType: "manual_harvesting_tools", itemID: 7 };
+    
+};
